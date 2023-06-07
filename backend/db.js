0af703872d9176e14c0452e8fd3a90228b10ba5c
ritikakatoch <ritikakatoch0128@gmail.com>
2023-06-08 02:32:21 +0530

    added

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const url =
+  "mongodb://127.0.0.1/mediumclone";
+module.exports.connect = () => {
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      // useFindAndModify: false,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+    })
+    .then(() => console.log("MongoDB is connected successfully"))
+    .catch((err) => console.log("Error: ", err));
+};
